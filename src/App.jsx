b4b5e7ad4b0ab38ae00c43539aa9b0285c9535ec
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-06-25 22:39:48 +0200

    adding state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-//import { useState } from 'react'
 import './App.css'
 import Calculator from './components/Calculator.jsx'
 
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,53 +1,89 @@
 
-function Button({ value }) {
+import { useState } from 'react'
+
+function Button({ value, handleInput }) {
   return (
-    <button className="calculator-button">
-      {value}
-    </button>
+    <>
+      <button className="calculator-button" onClick={() => handleInput(value)}>
+        {value}
+      </button>
+    </>
+
   );
 }
 
-function OutputScreen() {
+function OutputScreen({ input, result }) {
   return (
     <div className="output-screen">
-      <p>0</p>
-      <p className="result">Result</p>
+      <p>{input}</p>
+      <p className="result">{result}</p>
     </div>
   );
 }
 
 export default function Calculator() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  function handleInput(value) {
+    if (value === '=') {
+      console.log('Calculating result for:', parseFloat(input));
+    } else if (value === 'Clear') {
+      setInput('');
+      setResult('');
+    } else if (value === 'Delete') {
+      setInput(input.slice(0, -1));
+    } else {
+      //check if new input is valid
+      // the first input cannot be an oerator
+      // no consecutive operators allowed
+      // no operator at the end of the input
+
+      const lastChar = input.slice(-1);
+      if (input === '' && ['+', '-', '*', '/'].includes(value) && input.slice(-1) !== '.') {
+        return; // do not allow operator as first input
+      } else if (['+', '-', '*', '/'].includes(lastChar) && ['+', '-', '*', '/'].includes(value)) {
+        return; // do not allow consecutive operators
+      } else if (lastChar === '.' && ['+', '-', '*', '/'].includes(value)) {
+        return; // do not allow operator after decimal point
+      }
+      else {
+        setInput(input + value);
+      }
+    }
+  }
+
   return (
     <div className="calculator">
-      <h1>Calculator Component</h1>
-      <OutputScreen />
+      <h1>React Calculator</h1>
+      <OutputScreen input={input} result={result} />
       <div className="button-row">
-        <Button value="Clear" />
-        <Button value="Delete" />
-        <Button value="." />
-        <Button value="/" />
+        <Button value="Clear" handleInput={handleInput} />
+        <Button value="Delete" handleInput={handleInput} />
+        <Button value="." handleInput={handleInput} />
+        <Button value="/" handleInput={handleInput} />
       </div>
       <div className="button-row">
-        <Button value="7" />
-        <Button value="8" />
-        <Button value="9" />
-        <Button value="*" />
+        <Button value="7" handleInput={handleInput} />
+        <Button value="8" handleInput={handleInput} />
+        <Button value="9" handleInput={handleInput} />
+        <Button value="*" handleInput={handleInput} />
       </div>
       <div className="button-row">
-        <Button value="4" />
-        <Button value="5" />
-        <Button value="6" />
-        <Button value="-" />
+        <Button value="4" handleInput={handleInput} />
+        <Button value="5" handleInput={handleInput} />
+        <Button value="6" handleInput={handleInput} />
+        <Button value="-" handleInput={handleInput} />
       </div>
       <div className="button-row">
-        <Button value="1" />
-        <Button value="2" />
-        <Button value="3" />
-        <Button value="+" />
+        <Button value="1" handleInput={handleInput} />
+        <Button value="2" handleInput={handleInput} />
+        <Button value="3" handleInput={handleInput} />
+        <Button value="+" handleInput={handleInput} />
       </div>
       <div className="button-row">
-        <Button value="0" />
-        <Button value="=" />
+        <Button value="0" handleInput={handleInput} />
+        <Button value="=" handleInput={handleInput} />
       </div>
 
     </div>
